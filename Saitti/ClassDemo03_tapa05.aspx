aeb6825d624f8e3687a67258603ba08a97213d6b
Miika Avela <h4211@student.jamk.fi>
2016-09-22 10:54:59 +0300

    Class Demo 22.9.2016

---
+++ Saitti/ClassDemo03_tapa05.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ClassDemo03_tapa05.aspx.cs" Inherits="ClassDemo03_tapa05" %>
+<%@ PreviousPageType VirtualPath="~/ClassDemo03.aspx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Tapa 5: Tiedon välitys</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h1>Tapa 5: Public property</h1>
+        Teksti luettu kutsuvan sivun ominaisuudesta:
+        <div id="target" runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/ClassDemo03_tapa05.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ClassDemo03_tapa05 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Tsekataan tunnistaako edellisen sivun oikein
+        var joku = PreviousPage;
+        string msg = "";
+        if (joku != null)
+        {
+            // Tehdään jotain
+            msg = joku.Messu;
+            target.InnerHtml = msg;
+        }
+        else
+        {
+            target.InnerHtml = "Ei aiempaa sivua?!";
+        }
+    }
+}
